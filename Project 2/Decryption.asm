f032a87a104ebae7be5a05b479628d655781750d
Git-Senin <lawrencecole8@yahoo.com>
2022-12-08 09:02:25 -0800

    polish project 2

--- Project 2/Decryption.asm
+++ Project 2/Decryption.asm
@@ -8,7 +8,7 @@
 ExitProcess PROTO, dwExitCode:DWORD
 
 WriteConsoleA PROTO, hConsoleOutput:DWORD,lpBuffer:PTR BYTE,nNumberOfCharsToWrite:DWORD,lpNumberofCharsWritten:PTR DWORD,lpReserved:DWORD
-CreateFileA PROTO, lpFilename:PTR BYTE, dwDesiredAccess:DWORD, dwShareMode:DWORD, lpSecurityAttributes:DWORD, dwCreationDisposition:DWORD, dwFlagsAndAttributes:DWORD, hTemplateFile:DWORD
+CreateFileA PROTO, lpFilename:PTR BYTE, dwDESIredAccess:DWORD, dwShareMode:DWORD, lpSecurityAttributes:DWORD, dwCreationDisposition:DWORD, dwFlagsAndAttributes:DWORD, hTemplateFile:DWORD
 GetLastError PROTO
 GetStdHandle PROTO, nStdHandle:DWORD
 CloseHandle PROTO, hObject:DWORD
@@ -19,25 +19,23 @@ STD_INPUT_HANDLE    EQU -10
 STD_OUTPUT_HANDLE   EQU -11
 GENERIC_READ        EQU <80000000h>
 OPEN_EXISTING       EQU <3>
-NORMAL_FLAGS        EQU <128>
+NORMal_FLAGS        EQU <128>
 buffer              BYTE 300 DUP(?)
-
 filename            BYTE "encryption.txt",0
-ERROR_MESSAGE       BYTE "File does not exist.",0
-fileHandle          DWORD ?
-bytesRead           DWORD ?
+ERROR_MESSAGE       BYTE "The File does not exist.",0
 encryption_length   BYTE ?
 modulo              BYTE ?
+fileHandle          DWORD ?
+bytesRead           DWORD ?
 
 .code
 main PROC
-START_OF_PROGRAM:
-    INVOKE CreateFileA, ADDR filename, GENERIC_READ, 0, 0, OPEN_EXISTING, NORMAL_FLAGS, 0  ; Read File
-    mov fileHandle, eax
+    INVOKE CreateFileA, ADDR filename, GENERIC_READ, 0, 0, OPEN_EXISTING, NORMal_FLAGS, 0  ; Read File
+    MOV fileHandle, EAX
     INVOKE GetLastError
 
     ; If the text file does not exist, output an error message and quit
-    CMP eax, 2
+    CMP EAX, 2
     JNE FILE_EXISTS
 
         ; Error Message
@@ -49,10 +47,10 @@ START_OF_PROGRAM:
     ; If it does exist, read the first input length and modulo number from the text file
     FILE_EXISTS:
     INVOKE ReadFile, fileHandle, ADDR buffer, SIZEOF buffer, ADDR bytesRead, 0 ; read file into buffer
-    mov al, buffer[0]
-    mov encryption_length, al   ; read length of input
-    mov al, buffer[1]
-    mov modulo, al              ; read modulo number
+    MOV al, buffer[0]
+    MOV encryption_length, al   ; read length of input
+    MOV al, buffer[1]
+    MOV modulo, al              ; read modulo number
 
     ; If there is nothing to read, end the program
     CMP bytesRead, 0
@@ -61,14 +59,14 @@ START_OF_PROGRAM:
 
     ; Use the length of the input to read the first batch of input
     SOMETHING_READ:
-    mov esi, offset buffer  ; get input offset 
-    add esi, 2              ; from buffer offset
+    MOV ESI, OFFSET buffer  ; get input offset 
+    ADD ESI, 2              ; from buffer offset
 
     ; If the modulo number is 0, decrypt the data by flipping all of the bits of each character of the input
     CMP modulo, 0
     JNE MODULO_NOT_0
     
-    movzx eax, encryption_length
+    MOVZX EAX, encryption_length
     PUSH EAX                        ; Length
     PUSH ESI                        ; Offset for input
     CALL UndoFlippedBits            
@@ -76,18 +74,18 @@ START_OF_PROGRAM:
 
     ; If the modulo is not 0, decrypt the data by rolling each character to the right a number of times equal to the modulo
     MODULO_NOT_0:
-    movzx eax, modulo
-    push EAX                        ; modulo
-    movzx eax, encryption_length
-    push EAX                        ; Length
+    MOVZX EAX, modulo
+    PUSH EAX                        ; modulo
+    MOVZX EAX, encryption_length
+    PUSH EAX                        ; Length
     PUSH ESI                        ; Offset for input
     CALL UndoRolledString
 
     ; Write the decrypted input to the console
     WRITE_DECRYPTED_TO_CONSOLE:
-    movzx eax, encryption_length
-    push eax
-    PUSH ESI
+    MOVZX EAX, encryption_length
+    PUSH EAX                        ; length
+    PUSH ESI                        ; offset for input
     CALL WriteOutput
 
     ; Repeat from step 2
@@ -106,44 +104,44 @@ UndoRolledString PROC
 ;
 ; rolls each character to the left equal to the modulo
 ;
-; Receives: esi = offset
-;           ecx = length
+; Receives: ESI = offset
+;           ECX = length
 ;
 ; Returns: nothing
 ;------------------------------------------------------------------------
-    push ebp
-    mov ebp, esp
+    PUSH EBP
+    MOV EBP, ESP
 
-    push esi
-    push ecx   
-    push eax
-    push edx
+    PUSH ESI
+    PUSH ECX   
+    PUSH EAX
+    PUSH EDX
 ; Procedure
-    mov esi, [ebp + 8]  ; offset
-    mov ecx, [ebp + 12] ; length
-    mov edx, [ebp + 16] ; modulo amount
+    MOV ESI, [EBP + 8]  ; offset
+    MOV ECX, [EBP + 12] ; length
+    MOV EDX, [EBP + 16] ; modulo amount
 l1:            
-    mov al, [esi]   ; value to al
-    push ecx        ; save outer loop
+    MOV al, [ESI]   ; value to al
+    PUSH ECX        ; save outer loop
 
-    mov ecx, edx    ; set nest loop counter
+    MOV ECX, EDX    ; set nest loop counter
     roll_modulo:
         ror al, 1   ; roll char modulo times
     loop roll_modulo
-    mov [esi], al   ; set element to new bits
+    MOV [ESI], al   ; set element to new bits
 
-    pop ecx         ; return outer loop
-    inc esi         ; next element
+    pop ECX         ; return outer loop
+    inc ESI         ; next element
 loop l1
 
 ; End of Procedure
-    pop edx
-    pop eax
-    pop ecx
-    pop esi
+    pop EDX
+    pop EAX
+    pop ECX
+    pop ESI
 
-    mov esp, ebp
-    pop ebp
+    MOV ESP, EBP
+    pop EBP
     ret 12
 UndoRolledString ENDP
 ;------------------------------------------------------------------------
@@ -152,37 +150,37 @@ UndoFlippedBits PROC
 ;
 ; flips bits of each byte in string
 ;
-; Receives: esi = offset
-;           ecx = length
+; Receives: ESI = offset
+;           ECX = length
 ;
 ; Returns: nothing
 ;------------------------------------------------------------------------
-    push ebp
-    mov ebp, esp
+    PUSH EBP
+    MOV EBP, ESP
 
-    push esi
-    push ecx   
-    push eax
+    PUSH ESI
+    PUSH ECX   
+    PUSH EAX
 
 ; Procedure
 
-    mov esi, [ebp + 8]  ; offset
-    mov ecx, [ebp + 12] ; length
+    MOV ESI, [EBP + 8]  ; offset
+    MOV ECX, [EBP + 12] ; length
 l1:            
-    mov al, [esi]       ; Value to al
+    MOV al, [ESI]       ; Value to al
     xor al, 11111111b
-    mov [esi], al       ; ah to value
-    inc esi             ; next element
+    MOV [ESI], al       ; ah to value
+    inc ESI             ; next element
 loop l1
 
 ; End of Procedure
 
-    pop eax
-    pop ecx
-    pop esi
+    pop EAX
+    pop ECX
+    pop ESI
 
-    mov esp, ebp
-    pop ebp
+    MOV ESP, EBP
+    pop EBP
     ret 8
 UndoFlippedBits ENDP
 ;------------------------------------------------------------------------
@@ -191,43 +189,43 @@ WriteOutput PROC
 ;
 ; Writes a string to the console
 ;
-; Receives: [ebp + 8] = the pointer to the buffer where the output string is stored
-;           [ebp + 12] = the number of characters to write
+; Receives: [EBP + 8] = the pointer to the buffer where the output string is stored
+;           [EBP + 12] = the number of characters to write
 ;
 ; Returns: EAX = the number of bytes written
 ;------------------------------------------------------------------------
 
     ; Prologue - set up the stack frame
-    push ebp
-    mov ebp, esp
+    PUSH EBP
+    MOV EBP, ESP
 
     ; Local variables
-    ; [ebp - 4] = the output handle
-    ; [ebp - 8] = the number of bytes successfully written
-    sub esp, 8
+    ; [EBP - 4] = the output handle
+    ; [EBP - 8] = the number of bytes successfully written
+    sub ESP, 8
 
     ; Preserve registers
     ; Windows functions do not preserve EAX, EBX, ECX, or EDX
     ; They need to be preserved even if they go unused
-    push ebx
-    push ecx
-    push edx
+    PUSH EBX
+    PUSH ECX
+    PUSH EDX
 
     ; Procedure code
     invoke GetStdHandle, STD_OUTPUT_HANDLE                          ; get the output handle
-    mov [ebp - 4], eax                                              ; save the output handle in a local variable
+    MOV [EBP - 4], EAX                                              ; save the output handle in a local variable
 
-    lea edx, [ebp - 8]                                              ; EDX = pointer to variable holding bytes written
+    lea EDX, [EBP - 8]                                              ; EDX = pointer to variable holding bytes written
 
-    invoke WriteConsoleA, [ebp - 4], [ebp + 8], [ebp + 12], edx, 0   ; read the input
-    mov eax, [ebp - 8]                                              ; set return value
+    invoke WriteConsoleA, [EBP - 4], [EBP + 8], [EBP + 12], EDX, 0   ; read the input
+    MOV EAX, [EBP - 8]                                              ; set return value
 
     ; Epilogue - clean up the stack frame and return
-    pop edx
-    pop ecx
-    pop ebx
-    mov esp, ebp
-    pop ebp
+    pop EDX
+    pop ECX
+    pop EBX
+    MOV ESP, EBP
+    pop EBP
     ret 8
 WriteOutput ENDP
 ;------------------------------------------------------------------------
--- Project 2/Encryption.asm
+++ Project 2/Encryption.asm
@@ -9,10 +9,8 @@ ExitProcess PROTO, dwExitCode:DWORD
 
 ReadConsoleA PROTO, hConsoleInput:DWORD, lpbuffer: PTR BYTE, nNumberOfCharsToRead:DWORD,lpNumberOfCharsRead:PTR DWORD, lpReserved:DWORD
 WriteConsoleA PROTO, hConsoleOutput:DWORD,lpBuffer:PTR BYTE,nNumberOfCharsToWrite:DWORD,lpNumberofCharsWritten:PTR DWORD,lpReserved:DWORD
-
-CreateFileA PROTO, lpFilename:PTR BYTE, dwDesiredAccess:DWORD, dwShareMode:DWORD, lpSecurityAttributes:DWORD, dwCreationDisposition:DWORD, dwFlagsAndAttributes:DWORD, hTemplateFile:DWORD
+CreateFileA PROTO, lpFilename:PTR BYTE, dwDESIredAccess:DWORD, dwShareMode:DWORD, lpSecurityAttributes:DWORD, dwCreationDisposition:DWORD, dwFlagsAndAttributes:DWORD, hTemplateFile:DWORD
 WriteFile PROTO, hFile:DWORD, lpBuffer:PTR BYTE, nNumberOfBytesToWrite:DWORD, lpNumberOfBytesWritten:PTR DWORD, lpOverlapped:PTR DWORD
-
 GetStdHandle PROTO, nStdHandle:DWORD
 CloseHandle PROTO, hObject:DWORD
 
@@ -22,29 +20,29 @@ STD_OUTPUT_HANDLE   EQU -11
 GENERIC_WRITE       EQU <40000000h>
 ALWAYS_CREATE       EQU <2>
 NORMAL_FLAGS        EQU <128>
-length_of_input DWORD ?
-START_PROMPT    BYTE "Type anything of your choice: ",0
-buffer          BYTE 300 DUP(?)  
-amount_of_e     BYTE ?
-modulo          BYTE ?
-filename        BYTE "encryption.txt",0
-fileHandle      DWORD ?
-bytesWritten    DWORD ?
+buffer              BYTE 300 DUP(?)  
+START_PROMPT        BYTE "Type anything of your choice, or just 'q' to quit: ",0
+filename            BYTE "encryption.txt",0
+amount_of_e         BYTE ?
+modulo              BYTE ?
+fileHandle          DWORD ?
+bytesWritten        DWORD ?
+length_of_input     DWORD ?
 
 .code
 main PROC
-START_OF_PROGRAM:
     ; Tell the user to type anything of their choice, or just 'q' to quit.
     PUSH LENGTHOF START_PROMPT
     PUSH OFFSET START_PROMPT
     CALL WriteOutput
     
     ; Read the user's input
+    STEP_2:
     PUSH SIZEOF buffer  ; size of buffer
     PUSH OFFSET buffer  ; buffer offset
     CALL ReadInput
     SUB EAX, 2
-    mov length_of_input, eax  
+    MOV length_of_input, EAX  
 
     ; If the user entered just 'q', end the program
     CMP EAX, 1
@@ -57,20 +55,20 @@ START_OF_PROGRAM:
     PUSH LENGTHOF buffer
     PUSH OFFSET buffer
     CALL CountLetterE
-    mov amount_of_e, al
+    MOV amount_of_e, al
     
     ; Find the number of e's modulo 8 (the remainder when divided by 8)
-    movzx ax, al
-    mov bl, 8
+    MOVZX ax, al
+    MOV bl, 8
     DIV bl
-    mov modulo, ah
+    MOV modulo, ah
 
     ; If the modulo is 0, encrypt the data by flipping all of the bits of each character in the input
     CMP modulo, 0
     JNE MODULO_NOT_0 
 
-    ; Flip bits
-    mov eax, length_of_input    ; length
+        ; Flip bits
+    MOV EAX, length_of_input    ; length
     PUSH EAX                
     PUSH OFFSET buffer          ; offset
     CALL FlipBits
@@ -78,9 +76,9 @@ START_OF_PROGRAM:
 
     ; If the modulo is not 0, encrypt the data by rolling the bits of each character to the left a number of times equal to the modulo
     MODULO_NOT_0:
-    movzx eax, modulo           ; modulo
+    MOVZX EAX, modulo           ; modulo
     PUSH EAX
-    mov eax, length_of_input    ; length
+    MOV EAX, length_of_input    ; length
     PUSH EAX
     PUSH OFFSET buffer          ; offset
     CALL RollString
@@ -88,13 +86,13 @@ START_OF_PROGRAM:
     ; Write the length of the input, the modulo number, and then the encrypted input to the text file
     EXPORT_ENCRYPTION:
     INVOKE CreateFileA, ADDR filename, GENERIC_WRITE, 0, 0, ALWAYS_CREATE, NORMAL_FLAGS, 0  ; Create File
-    mov fileHandle, eax
+    MOV fileHandle, EAX
     INVOKE WriteFile, fileHandle, ADDR length_of_input, 1, ADDR bytesWritten, 0             ; Write length of input
     INVOKE WriteFile, fileHandle, ADDR modulo, 1,           ADDR bytesWritten, 0            ; Write modulo number
     INVOKE WriteFile, fileHandle, ADDR buffer, length_of_input, ADDR bytesWritten, 0        ; Write encrypted Input
 
     ; Repeat from step 2
-    JMP START_OF_PROGRAM
+    JMP STEP_2
 END_OF_PROGRAM:
     INVOKE CloseHandle, fileHandle
     INVOKE ExitProcess, 0
@@ -109,44 +107,44 @@ RollString PROC
 ;
 ; rolls each character to the left equal to the modulo
 ;
-; Receives: esi = offset
-;           ecx = length
+; Receives: ESI = offset
+;           ECX = length
 ;
 ; Returns: nothing
 ;------------------------------------------------------------------------
-    push ebp
-    mov ebp, esp
+    push EBP
+    MOV EBP, ESP
 
-    push esi
-    push ecx   
-    push eax
-    push edx
+    push ESI
+    push ECX   
+    push EAX
+    push EDX
 ; Procedure
-    mov esi, [ebp + 8]  ; offset
-    mov ecx, [ebp + 12] ; length
-    mov edx, [ebp + 16] ; modulo amount
+    MOV ESI, [EBP + 8]  ; offset
+    MOV ECX, [EBP + 12] ; length
+    MOV EDX, [EBP + 16] ; modulo amount
 l1:            
-    mov al, [esi]   ; value to al
-    push ecx        ; save outer loop
+    MOV al, [ESI]   ; value to al
+    push ECX        ; save outer loop
 
-    mov ecx, edx    ; set nest loop counter
+    MOV ECX, EDX    ; set nest loop counter
     roll_modulo:
         rol al, 1   ; roll char modulo times
     loop roll_modulo
-    mov [esi], al   ; set element to new bits
+    MOV [ESI], al   ; set element to new bits
 
-    pop ecx         ; return outer loop
-    inc esi         ; next element
+    pop ECX         ; return outer loop
+    inc ESI         ; next element
 loop l1
 
 ; End of Procedure
-    pop edx
-    pop eax
-    pop ecx
-    pop esi
+    pop EDX
+    pop EAX
+    pop ECX
+    pop ESI
 
-    mov esp, ebp
-    pop ebp
+    MOV ESP, EBP
+    pop EBP
     ret 12
 RollString ENDP
 ;------------------------------------------------------------------------
@@ -155,37 +153,37 @@ FlipBits PROC
 ;
 ; flips bits of each byte in string
 ;
-; Receives: esi = offset
-;           ecx = length
+; Receives: ESI = offset
+;           ECX = length
 ;
 ; Returns: nothing
 ;------------------------------------------------------------------------
-    push ebp
-    mov ebp, esp
+    push EBP
+    MOV EBP, ESP
 
-    push esi
-    push ecx   
-    push eax
+    push ESI
+    push ECX   
+    push EAX
 
 ; Procedure
 
-    mov esi, [ebp + 8]  ; offset
-    mov ecx, [ebp + 12] ; length
+    MOV ESI, [EBP + 8]  ; offset
+    MOV ECX, [EBP + 12] ; length
 l1:            
-    mov al, [esi]       ; Value to al
+    MOV al, [ESI]       ; Value to al
     xor al, 11111111b
-    mov [esi], al       ; ah to value
-    inc esi             ; next element
+    MOV [ESI], al       ; ah to value
+    inc ESI             ; next element
 loop l1
 
 ; End of Procedure
 
-    pop eax
-    pop ecx
-    pop esi
+    pop EAX
+    pop ECX
+    pop ESI
 
-    mov esp, ebp
-    pop ebp
+    MOV ESP, EBP
+    pop EBP
     ret 8
 FlipBits ENDP
 ;------------------------------------------------------------------------
@@ -194,37 +192,37 @@ CountLetterE PROC
 ;
 ; Counts and returns 'e's in given string
 ;
-; Receives: esi = offset
-;           ecx = length
+; Receives: ESI = offset
+;           ECX = length
 ;
 ; Returns: AL = amount of e's in String
 ;------------------------------------------------------------------------
 
-    push ebp
-    mov ebp, esp
+    push EBP
+    MOV EBP, ESP
 
-    push esi
-    push ecx   
+    push ESI
+    push ECX   
 ; Procedure
 
-    mov esi, [ebp + 8]  ; offset
-    mov ecx, [ebp + 12] ; length
-    mov al, 0
+    MOV ESI, [EBP + 8]  ; offset
+    MOV ECX, [EBP + 12] ; length
+    MOV al, 0
 l1:
-    mov bl, BYTE PTR [esi] ; get char
+    MOV bl, BYTE PTR [ESI] ; get char
     CMP bl, 'e'
     JNE NEXT_INDEX
     inc al
     NEXT_INDEX:
-    inc esi ; next element
+    inc ESI ; next element
 loop l1
 
 ; End of Procedure
-    pop ecx
-    pop esi
+    pop ECX
+    pop ESI
 
-    mov esp, ebp
-    pop ebp
+    MOV ESP, EBP
+    pop EBP
     ret 8
 CountLetterE ENDP
 ;------------------------------------------------------------------------
@@ -233,44 +231,44 @@ ReadInput PROC
 ;
 ; Reads a string from the console
 ;
-; Receives: [ebp + 8] = the pointer to the buffer where the input string will be stored
-;           [ebp + 12] = the size limit of the buffer
+; Receives: [EBP + 8] = the pointer to the buffer where the input string will be stored
+;           [EBP + 12] = the size limit of the buffer
 ; (make sure to leave 2 spaces for the end-of-line characters)
 ;
 ; Returns: EAX = the number of bytes read during the input
 ;------------------------------------------------------------------------
 
     ; Prologue - set up the stack frame
-    push ebp
-    mov ebp, esp
+    push EBP
+    MOV EBP, ESP
 
     ; Local variables
-    ; [ebp - 4] = the input handle
-    ; [ebp - 8] = the number of bytes successfully read
-    sub esp, 8
+    ; [EBP - 4] = the input handle
+    ; [EBP - 8] = the number of bytes successfully read
+    sub ESP, 8
 
     ; Preserve registers
     ; Windows functions do not preserve EAX, EBX, ECX, or EDX
     ; They need to be preserved even if they go unused
-    push ebx
-    push ecx
-    push edx
+    push EBX
+    push ECX
+    push EDX
 
     ; Procedure code
     invoke GetStdHandle, STD_INPUT_HANDLE                               ; get the input handle
-    mov [ebp - 4], eax                                                  ; save the input handle in a local variable
+    MOV [EBP - 4], EAX                                                  ; save the input handle in a local variable
 
-    lea edx, [ebp - 8]                                                  ; EDX = pointer to variable holding bytes read
+    lea EDX, [EBP - 8]                                                  ; EDX = pointer to variable holding bytes read
 
-    invoke ReadConsoleA, [ebp - 4], [ebp + 8], [ebp + 12], edx, 0       ; read the input
-    mov eax, [ebp - 8]                                                  ; set return value
+    invoke ReadConsoleA, [EBP - 4], [EBP + 8], [EBP + 12], EDX, 0       ; read the input
+    MOV EAX, [EBP - 8]                                                  ; set return value
 
     ; Epilogue - clean up stack frame and return
-    pop edx
-    pop ecx
-    pop ebx
-    mov esp, ebp
-    pop ebp
+    pop EDX
+    pop ECX
+    pop EBX
+    MOV ESP, EBP
+    pop EBP
     ret 8
 ReadInput ENDP
 ;------------------------------------------------------------------------
@@ -279,43 +277,43 @@ WriteOutput PROC
 ;
 ; Writes a string to the console
 ;
-; Receives: [ebp + 8] = the pointer to the buffer where the output string is stored
-;           [ebp + 12] = the number of characters to write
+; Receives: [EBP + 8] = the pointer to the buffer where the output string is stored
+;           [EBP + 12] = the number of characters to write
 ;
 ; Returns: EAX = the number of bytes written
 ;------------------------------------------------------------------------
 
     ; Prologue - set up the stack frame
-    push ebp
-    mov ebp, esp
+    push EBP
+    MOV EBP, ESP
 
     ; Local variables
-    ; [ebp - 4] = the output handle
-    ; [ebp - 8] = the number of bytes successfully written
-    sub esp, 8
+    ; [EBP - 4] = the output handle
+    ; [EBP - 8] = the number of bytes successfully written
+    sub ESP, 8
 
     ; Preserve registers
     ; Windows functions do not preserve EAX, EBX, ECX, or EDX
     ; They need to be preserved even if they go unused
-    push ebx
-    push ecx
-    push edx
+    push EBX
+    push ECX
+    push EDX
 
     ; Procedure code
     invoke GetStdHandle, STD_OUTPUT_HANDLE                          ; get the output handle
-    mov [ebp - 4], eax                                              ; save the output handle in a local variable
+    MOV [EBP - 4], EAX                                              ; save the output handle in a local variable
 
-    lea edx, [ebp - 8]                                              ; EDX = pointer to variable holding bytes written
+    lea EDX, [EBP - 8]                                              ; EDX = pointer to variable holding bytes written
 
-    invoke WriteConsoleA, [ebp - 4], [ebp + 8], [ebp + 12], edx, 0   ; read the input
-    mov eax, [ebp - 8]                                              ; set return value
+    invoke WriteConsoleA, [EBP - 4], [EBP + 8], [EBP + 12], EDX, 0   ; read the input
+    MOV EAX, [EBP - 8]                                              ; set return value
 
     ; Epilogue - clean up the stack frame and return
-    pop edx
-    pop ecx
-    pop ebx
-    mov esp, ebp
-    pop ebp
+    pop EDX
+    pop ECX
+    pop EBX
+    MOV ESP, EBP
+    pop EBP
     ret 8
 WriteOutput ENDP
 ;------------------------------------------------------------------------
